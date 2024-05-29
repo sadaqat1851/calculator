095cd1bd3287bb7cd78f9114c39390eab248f068
Sadaqathocein <sadaqathocein72@gmail.com>
2024-05-29 23:19:56 +0500

    Calculator

---
+++ script.js
@@ -0,0 +1,21 @@
+const display = document.getElementById("display")
+
+
+function appendToDisplay(Input) {
+    display.value += Input;
+}
+
+
+function clearDisplay(){
+    display.value = "";
+
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    }
+    catch (error) {
+        display.value = "Error";
+    }
+}
